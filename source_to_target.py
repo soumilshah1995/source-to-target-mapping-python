f6b1b8f16f472124970fbf13accb58246c311ba6
Soumil Nitin Shah <39345855+soumilshah1995@users.noreply.github.com>
2022-10-26 19:00:05 -0400

    Update source_to_target.py

--- source_to_target.py
+++ source_to_target.py
@@ -1,215 +1,230 @@
+from abc import ABC, abstractmethod
+
 from jsonpath_ng import parse
 
-"""Items which will be stored in database """
-json_data = {
-    "source": [
-        {
-            "id": 1,
-            "source_field_name": "id",
-            "source_field_mapping": "$.id",
-            "source_field_type": "str",
-            "is_required": True
-        }
-    ],
-    "destination": [
-        {
-            "id": 1,
-            "destination_field_name": "customerID",
-            "destination_field_mapping": "customerID",
-            "destination_field_type": "str",
-            "default_value": -1
-        }
-    ],
-    "mapping": [
-        {
-            "mapping_id": 1,
-            "mapping_source": 1,
-            "mapping_destination": 1
-        }
-    ]
-}
 
-json_incoming_data = {
-    "id":"12213"
-}
+class Interface(ABC):
+
+    @abstractmethod
+    def get_data_by_field(self, field_name):
+        """Fetch the data by given feild name """
+
+    @abstractmethod
+    def get_data_by_id(self, id):
+        """Fetch the data by given ID  """
+
+    @abstractmethod
+    def get(self):
+        """Fetch all data """
 
 
-class Source(object):
-
+class Database(object):
     def __init__(self):
         pass
 
-    def get_data_field(self, field_name):
-        data = self.fetch_data()
+    @property
+    def get_data_source_target_mapping(self):
+        return {
+            "source": [
+                {
+                    "id": 1,
+                    "source_field_name": "id",
+                    "source_field_mapping": "$.id",
+                    "source_field_type": "str",
+                    "is_required": True
+                }
+            ],
+            "destination": [
+                {
+                    "id": 1,
+                    "destination_field_name": "customerID",
+                    "destination_field_mapping": "customerID",
+                    "destination_field_type": "str",
+                    "default_value": -1
+                }
+            ],
+            "mapping": [
+                {
+                    "id": 1,
+                    "mapping_source": 1,
+                    "mapping_destination": 1
+                }
+            ]
+        }
+
+
+class Source(Interface, Database):
+    def __init__(self):
+        Database.__init__(self)
+
+    def get_data_by_field(self, field_name):
+        data = self.get
         for item in data:
             for key, value in item.items():
                 if key == field_name:
                     return item
         return None
 
-    def fetch_data(self):
-        return json_data.get("source")
+    @property
+    def get(self):
+        return self.get_data_source_target_mapping.get("source")
 
-    def get_data_id(self, id):
+    def get_data_by_id(self, id):
         self.id = id
-        data = self.fetch_data()
+        data = self.get
         for x in data:
             if x.get("id") == self.id:
                 return x
         return None
 
 
-class Target(object):
-    def __init__(self):
-        pass
+class Target(Interface, Database):
 
-    def get_data_field(self, field_name):
-        data = self.fetch_data()
+    def __init__(self):
+        Database.__init__(self)
+
+    def get_data_by_field(self, field_name):
+        data = self.get
         for item in data:
             for key, value in item.items():
                 if key == field_name:
                     return item
         return None
 
-    def fetch_data(self):
-        return json_data.get("destination")
+    @property
+    def get(self):
+        return self.get_data_source_target_mapping.get("destination")
 
-    def get_data_id(self, id):
+    def get_data_by_id(self, id):
         self.id = id
-        data = self.fetch_data()
+        data = self.get
         for x in data:
             if x.get("id").__str__() == self.id.__str__():
                 return x
         return None
 
 
-class Mappings(object):
+class Mappings(Interface, Database):
 
     def __init__(self):
-        pass
+        Database.__init__(self)
 
-    def fetch_data(self):
-        return json_data.get("mapping")
+    @property
+    def get(self):
+        return self.get_data_source_target_mapping.get("mapping")
+
+    def get_data_by_id(self, id):
+        self.id = id
+        data = self.get
+        for x in data:
+            if x.get("id").__str__() == self.id.__str__():
+                return x
+        return None
+
+    def get_data_by_field(self, field_name):
+        return None
 
 
-mapping_instance = Mappings()
-mapping_data = mapping_instance.fetch_data()
+class JsonQuery(object):
 
-source_instance = Source()
-destination_instance = Target()
+    def __init__(self, json_path, json_data):
+        self.json_path = json_path
+        self.json_data = json_data
 
-transformed_data = []
-json_data_transformed = {}
-
-for mappings in mapping_data:
-
-    """fetch the source mapping """
-    mapping_source_id = mappings.get("mapping_source")
-    mapping_destination_id = mappings.get("mapping_destination")
-
-    mapping_source_data = source_instance.get_data_id(mapping_source_id)
-
-    """Fetch Source  field Name"""
-    source_field_name = mapping_source_data.get("source_field_name")
-
-    if source_field_name not in json_incoming_data.keys():
-        if mapping_source_data.get("is_required"):
-            print("Alert ! Field {} is not present in JSON please FIX mappings ".format(source_field_name))
-            raise Exception("Alert ! Field {} is not present in JSON please FIX mappings ".format(source_field_name))
-
-    else:
-        source_json_path = mapping_source_data.get("source_field_mapping")
-
-        """Query the JSON based on PATh"""
-        jsonpath_expression = parse(source_json_path)
-
-        match = jsonpath_expression.find(json_incoming_data)
+    def get(self):
+        jsonpath_expression = parse(self.json_path)
+        match = jsonpath_expression.find(self.json_data)
         source_data_value = match[0].value
+        return source_data_value
 
-        """check the data type for source if matches with what we have previous """
-        if mapping_source_data.get("source_field_type") != type(source_data_value).__name__:
 
-            if source_data_value is not None:
-                _message = ("Alert ! Source Field :{} Datatype has changed from {} to {} ".format(source_field_name,
+class Transform(object):
+    def __init__(self, input_json):
+
+        self.input_json = input_json
+
+        self.mapping_instance = Mappings()
+        self.source_instance = Source()
+        self.destination_instance = Target()
+        self.json_data_transformed = {}
+
+    def _get_mapping_data(self):
+        return self.mapping_instance.get
+
+    def _get_mapping_source_data(self):
+        return self.source_instance.get
+
+    def get_transformed_data(self):
+
+        for mappings in self._get_mapping_data():
+
+            """fetch the source mapping """
+            mapping_source_id = mappings.get("mapping_source")
+            mapping_destination_id = mappings.get("mapping_destination")
+
+            mapping_source_data = self.source_instance.get_data_by_id(id=mapping_source_id)
+
+            """Fetch Source  field Name"""
+            source_field_name = mapping_source_data.get("source_field_name")
+
+            """if field given is not present incoming json """
+            if source_field_name not in self.input_json.keys():
+                if mapping_source_data.get("is_required"):
+                    print("Alert ! Field {} is not present in JSON please FIX mappings ".format(source_field_name))
+                    raise Exception(
+                        "Alert ! Field {} is not present in JSON please FIX mappings ".format(source_field_name))
+            else:
+                source_data_value = JsonQuery(
+                    json_path=mapping_source_data.get("source_field_mapping"),
+                    json_data=self.input_json
+                ).get()
+
+                """check the data type for source if matches with what we have previous """
+                if mapping_source_data.get("source_field_type") != type(source_data_value).__name__:
+                    if source_data_value is not None:
+                        _message = (
+                            "Alert ! Source Field :{} Datatype has changed from {} to {} ".format(source_field_name,
                                                                                                   mapping_source_data.get(
                                                                                                       "source_field_type"),
                                                                                                   type(
-                                                                                                      source_data_value).__name__
-                                                                                                  ))
-                print(_message)
-                raise Exception(_message)
+                                                                                                      source_data_value).__name__))
+                        print(_message)
+                        raise Exception(_message)
 
-        """Query and fetch the Destination | target """
-        destination_mappings_json_object = destination_instance.get_data_id(mapping_destination_id)
-        destination_field_name = destination_mappings_json_object.get("destination_field_name")
+                """Query and fetch the Destination | target """
+                destination_mappings_json_object = self.destination_instance.get_data_by_id(
+                    id=mappings.get("mapping_destination"))
+                destination_field_name = destination_mappings_json_object.get("destination_field_name")
+                destination_field_type = destination_mappings_json_object.get("destination_field_type")
 
-        destination_field_type = destination_mappings_json_object.get("destination_field_type")
+                dtypes = [str, float, list, int, set]
 
-        """Datatype Conversion """
-        if destination_field_type == "str":
+                for dtype in dtypes:
 
-            """is source is none iinsert default value"""
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = source_data_value
-                transformed_data.append(json_data_transformed)
+                    """Datatype Conversion """
+                    if destination_field_type == str(dtype.__name__):
 
-        if destination_field_type == "float":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = float(source_data_value)
-                transformed_data.append(json_data_transformed)
+                        """is source is none insert default value"""
+                        if source_data_value is None:
+                            self.json_data_transformed[destination_field_name] = dtype(
+                                destination_mappings_json_object.get("default_value")
+                            )
+                        else:
+                            self.json_data_transformed[destination_field_name] = source_data_value
 
-        if destination_field_type == "int":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = int(source_data_value)
-                transformed_data.append(json_data_transformed)
+        print(self.json_data_transformed)
 
-        if destination_field_type == "list":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = list(source_data_value)
-                transformed_data.append(json_data_transformed)
 
-        if destination_field_type == "set":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = set(source_data_value)
-                transformed_data.append(json_data_transformed)
+def main():
 
-        if destination_field_type == "bool":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = bool(source_data_value)
-                transformed_data.append(json_data_transformed)
+    json_incoming_data = {
+        "id": "12213"
+    }
 
-        if destination_field_type == "bytes":
-            if source_data_value is None:
-                json_data_transformed[destination_field_name] = destination_mappings_json_object.get("default_value")
-                transformed_data.append(json_data_transformed)
-            else:
-                json_data_transformed[destination_field_name] = bytes(source_data_value)
-                transformed_data.append(json_data_transformed)
+    helper = Transform(input_json=json_incoming_data)
+    response = helper.get_transformed_data()
 
-print(f"""
+    print(response)
 
-    ====================SOURCE=====================
-                {json_incoming_data}
-    ====================Transformed================
-             {transformed_data[0]}
-    =================================================
-    
-""")
+
+main()
